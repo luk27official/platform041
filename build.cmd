18bcdd4866c75c7fc902263b99064fa72cca2bbb
Lukas Polak <admin@lukaspolak.cz>
2023-01-25 12:35:57 +0100

    added simple build system with cmake

---
+++ build.cmd
@@ -0,0 +1,2 @@
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles"
+cmake --build build
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
 }
